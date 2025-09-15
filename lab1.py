5126389f150f0464d7cfe3199d5b10380d20b5a0
lekswell <leks.kuzmin29@mail.ru>
2025-09-15 17:57:19 +0300

    lab1.py - программа обработки последовательности чисел

---
+++ lab1.py
@@ -0,0 +1,21 @@
+#  На языке Python, C# или C++ (в консольном режиме) напишите программу обработки последовательности чисел.
+# Сначала программа запрашивает число N. Затем создает матрицу D(N, N)  из случайных целых чисел (не превышающих
+# по модулю 105). Затем определяет среднее значение среди положительных элементов и формирует массив М
+# уменьшением элементов из D на это найденное значение.
+# Выходные данные – исходная матрица D и матрица M, полученная после перестановки.
+import random
+N = int(input())
+sum = 0
+count = 0
+matrix = [[random.randint(-5,5) for i in range(N)] for i in range(N)]
+for i in range(N):
+  for j in range(N):
+    if matrix[i][j] > 0:
+      count += 1
+      sum += matrix[i][j]
+mid = sum/count
+for i in range(N):
+  for j in range(N):
+    matrix[i][j] -= mid
+for row in matrix:
+  print(row)
